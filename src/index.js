4f2c4ca7078c0a423fd28ebb1f2914a2358b9a2b
Dany <danilukas@mail.ru>
2019-11-25 13:57:46 +0300

    update webpack config

--- src/index.js
+++ src/index.js
@@ -1,48 +1,5 @@
 "use strict";
-function slider() {
-    let slideIndex = 1,
-    slides = document.querySelectorAll(".slider__item"),
-    prev = document.querySelector(".prev"),
-    next = document.querySelector(".next"),
-    dotswrap = document.querySelector(".slider-dots"),
-    dot = document.querySelectorAll(".dot");
 
-
-ShowSlides(slideIndex)
-function ShowSlides(n) {
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-
-    slides.forEach((item) => item.style.display = "none");
-    dot.forEach((item) => item.classList.remove("dot-active"))
-    slides[slideIndex - 1].style.display = "block";
-    dot[slideIndex - 1].classList.add("dot-active");
-}
-function plus(n) {
-    ShowSlides(slideIndex += n);
-};
-function SlidesBtn(n) {
-    ShowSlides(slideIndex = n);
-};
-prev.addEventListener("click", function(){
-    plus(-1);
-});
-next.addEventListener("click", function(){
-    plus(1);
-});
-dotswrap.addEventListener("click", function(event) {
-    for(let i = 0; i < dot.length + 1; i++) {
-        if(event.target.classList.contains("dot") && event.target == dot[i-1]) {
-            SlidesBtn(i);
-        }
-    }
-});
-}
-slider();
 
 //tabs 
 let link = document.querySelectorAll(".catalog-item__link"),
@@ -59,7 +16,7 @@ function linkWrap(d) {
         link[i].addEventListener("click", function() {
             list[i].classList.add("catalog-item__list_active");
             content[i].classList.remove("catalog-item__content_active");
-        })
+        });
     }
 }
 
@@ -89,7 +46,7 @@ function hideTabs(b) {
 function run() {
     tabs.forEach(function(tabs) {
         tabs.classList.remove("catalog__content_active");
-    })
+    });
 }
 
 catalog.addEventListener("click", function(event) {
@@ -113,9 +70,9 @@ catalog.addEventListener("click", function(event) {
             }
         }
     }
-})
-
-import "./css/bootstrap-grid.min.css"
-import "./css/bootstrap-reboot.min.css"
-import "./css/fonts.css"
-import "./scss/main.scss"
+});
+import "./js/modules/slider.js";
+import "./css/bootstrap-grid.min.css";
+import "./css/bootstrap-reboot.min.css";
+import "./css/fonts.css";
+import "./scss/main.scss";
---
+++ src/js/modules/slider.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function slider() {
+    let slideIndex = 1,
+    slides = document.querySelectorAll(".slider__item"),
+    prev = document.querySelector(".prev"),
+    next = document.querySelector(".next"),
+    dotswrap = document.querySelector(".slider-dots"),
+    dot = document.querySelectorAll(".dot");
+
+
+ShowSlides(slideIndex)
+function ShowSlides(n) {
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => item.style.display = "none");
+    dot.forEach((item) => item.classList.remove("dot-active"));
+    slides[slideIndex - 1].style.display = "block";
+    dot[slideIndex - 1].classList.add("dot-active");
+}
+function plus(n) {
+    ShowSlides(slideIndex += n);
+}
+function SlidesBtn(n) {
+    ShowSlides(slideIndex = n);
+}
+prev.addEventListener("click", () => {
+    plus(-1);
+});
+next.addEventListener("click", () => {
+    plus(1);
+});
+dotswrap.addEventListener("click", function(event) {
+    for(let i = 0; i < dot.length + 1; i++) {
+        if(event.target.classList.contains("dot") && event.target == dot[i-1]) {
+            SlidesBtn(i);
+        }
+    }
+});
+}
+slider();
